fcd30e24be4405a6992f478164907a751e5641cb
Tsvetomir Stoev <tsvetomirstoev8@gmail.com>
2021-09-09 19:42:27 +0300

    Styled the page and finished the fetching feature

--- src/components/Dorpdown.tsx
+++ src/components/Dorpdown.tsx
@@ -1,14 +1,74 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import React, { useState } from "react";
 
+import GroupBox from "./GroupBox/GroupBox";
 import "./Dropdown.css";
 
+type objectType = { key: string; value: number };
 const Dropdown = () => {
+  const [display1, setDisplay1] = useState<JSX.Element>(),
+    [display2, setDisplay2] = useState<JSX.Element>(),
+    [display3, setDisplay3] = useState<JSX.Element>();
 
-    const cl = ()=>{
-        console.log("clicked");
-        
-    };
+  const cl = async (currName: string) => {
+    const cacheName = `currency-1`;
+    const baseUrl =
+      "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+    const currencyCodes = ["usd", "eur", "aud", "cad", "chf", "nzd", "bgn"];
+
+    let url = baseUrl + "/" + currName + ".json";
+    getCachedData(cacheName, url);
+
+    async function getCachedData(cacheName: any, url: any) {
+      const cacheStorage = await caches.open(cacheName);
+      const cachedResponse = await cacheStorage.match(url);
+
+      if (!cachedResponse || !cachedResponse.ok) {
+        return false;
+      }
+      let data: any = await cachedResponse.json();
+
+      let group1: objectType = {} as objectType;
+      let group2: objectType = {} as objectType;
+      let group3: objectType = {} as objectType;
+
+      for (let i of currencyCodes) {
+        if (currName !== i) {
+          if (data[`${currName}`][`${i}`] < 1) {
+            Object.defineProperty(
+              group1,
+              `${currName.toUpperCase()}-${i.toUpperCase()}`,
+              { value: data[`${currName}`][`${i}`] }
+            );
+          }
+          if (
+            data[`${currName}`][`${i}`] >= 1 &&
+            data[`${currName}`][`${i}`] < 1.5
+          ) {
+            Object.defineProperty(
+              group2,
+              `${currName.toUpperCase()}-${i.toUpperCase()}`,
+              { value: data[`${currName}`][`${i}`] }
+            );
+          }
+          if (data[`${currName}`][`${i}`] >= 1.5) {
+            Object.defineProperty(
+              group3,
+              `${currName.toUpperCase()}-${i.toUpperCase()}`,
+              { value: data[`${currName}`][`${i}`] }
+            );
+          }
+        }
+      }
+
+      //  console.log(clickedData);
+
+      setDisplay1(<GroupBox boxNumber={1} boxData={group1} />);
+      setDisplay2(<GroupBox boxNumber={2} boxData={group2} />);
+      setDisplay3(<GroupBox boxNumber={3} boxData={group3} />);
+    }
+  };
+
   return (
     <div className="dropdown-container">
       <div className="wrap">
@@ -19,30 +79,77 @@ const Dropdown = () => {
               <a>Currencies</a>
               <ul className="sub">
                 <li>
-                  <a onClick={cl}>USD</a>
+                  <a
+                    onClick={() => {
+                      cl("usd");
+                    }}
+                  >
+                    USD
+                  </a>
                 </li>
                 <li>
-                  <a>EUR</a>
+                  <a
+                    onClick={() => {
+                      cl("eur");
+                    }}
+                  >
+                    EUR
+                  </a>
                 </li>
                 <li>
-                  <a>AUD</a>
+                  <a
+                    onClick={() => {
+                      cl("aud");
+                    }}
+                  >
+                    AUD
+                  </a>
                 </li>
                 <li>
-                  <a>CAD</a>
+                  <a
+                    onClick={() => {
+                      cl("cad");
+                    }}
+                  >
+                    CAD
+                  </a>
                 </li>
                 <li>
-                  <a>CHF</a>
+                  <a
+                    onClick={() => {
+                      cl("chf");
+                    }}
+                  >
+                    CHF
+                  </a>
                 </li>
                 <li>
-                  <a>NZD</a>
+                  <a
+                    onClick={() => {
+                      cl("nzd");
+                    }}
+                  >
+                    NZD
+                  </a>
                 </li>
                 <li>
-                  <a>BGN</a>
+                  <a
+                    onClick={() => {
+                      cl("bgn");
+                    }}
+                  >
+                    BGN
+                  </a>
                 </li>
               </ul>
             </li>
           </ul>
         </nav>
+      </div>
+      <div className="display-container">
+        {display1}
+        {display2}
+        {display3}
       </div>
     </div>
   );
--- src/components/GroupBox/GroupBox.tsx
+++ src/components/GroupBox/GroupBox.tsx
@@ -2,11 +2,23 @@ import React from "react";
 
 import './GroupBox.css';
 
-const GroupBox = ()=>{
-
-
+type BoxProps = {
+    boxNumber:number,
+    boxData:object
+}
+const GroupBox = ({boxNumber,boxData}:BoxProps)=>{
+    
+    console.log(Object.getOwnPropertyNames(boxData));
+    
     return(
-        <div className="box-container"></div>
+        <div className="box-container">
+            <h1 className="box-header">Group {boxNumber}</h1>
+            {Object.keys(boxData).map((e:any,i:number)=>{
+                   return(
+                       <h2>{i}</h2>
+                   )
+            })}
+        </div>
     );
 };
 export default GroupBox;
